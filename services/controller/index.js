96ce51c097c929383a7012d471ea0e14171e6334
uBeny <benylsonsilva@gmail.com>
2025-12-02 01:23:20 -0400

    Tarefa prática #2 - Serviço de Avaliações

--- services/controller/index.js
+++ services/controller/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const shipping = require('./shipping');
 const inventory = require('./inventory');
+const reviews = require('../review/review');
 const cors = require('cors');
 
 const app = express();
@@ -20,6 +21,7 @@ app.get('/products', (req, res, next) => {
         }
     });
 });
+
 
 /**
  * Consulta o frete de envio no ShippingService
@@ -56,6 +58,37 @@ app.get('/product/:id', (req, res, next) => {
             // microsserviço (um arquivo JSON) com os dados
             // do produto pesquisado
             res.json(product);
+        }
+    });
+});
+
+// Rota para obter avaliações de um produto
+app.get('/reviews/:id', (req, res, next) => {
+    reviews.GetReviews({ id: req.params.id }, (err, reviewsData) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send({ error: 'Falha ao recuperar avaliações' });
+        } else {
+            res.json(reviewsData);
+        }
+    });
+});
+
+// Rota para adicionar uma nova avaliação
+app.post('/reviews', (req, res, next) => {
+    const review = {
+        productId: req.body.productId,
+        username: req.body.username,
+        rating: req.body.rating,
+        comment: req.body.comment,
+    };
+
+    reviews.AddReview(review, (err, response) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send({ error: 'Falha ao adicionar avaliação' });
+        } else {
+            res.json(response);
         }
     });
 });
